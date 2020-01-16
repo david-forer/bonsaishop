ddbeed91e3cddfb4baa420f95743b723e5515fa5
david-forer <david.j.forer@gmail.com>
2020-01-16 17:59:46 +0900

    starting adding to cli

--- lib/bonsaishop/cli.rb
+++ lib/bonsaishop/cli.rb
@@ -1,6 +1,23 @@
 class Bonsaishop::CLI
 
     def start
-        puts "Howdy Neighbor"
+        puts "Welcome to The Bonsai Shop!"
+        puts "Please choose a type of Bonsai Plant to look at:"
+        get_type_of_plants
+        get_user_plants
+        #user
+        
+    end
+
+    def get_type_of_plants
+        #scrape this
+        @plants = ['Indoor Plants', 'Outdoor Plants', 'Flowering Plants', 'Speciman Plants', 'Starter Plants']
+    end
+
+    def get_user_plants
+        #list_plants
+        @plants.each.with_index {|plant, index| 
+          puts  "#{index}. #{plant}"
+        }
     end
 end
